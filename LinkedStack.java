95529fb96230339f487931c0649c59c8f4284a39
alsmirnova <alyona_smir@mail.ru>
2016-11-14 23:07:56 +0300

    LinkedStack

---
+++ LinkedStack.java
@@ -0,0 +1,73 @@
+package seminar1.collections;
+
+import java.util.Iterator;
+
+public class LinkedStack<Item> implements IStack<Item> {
+
+    private Node<Item> head;
+    private int size;
+
+    @Override
+    public void push(Item item) {
+        /* TODO: implement it */
+        head.next = head;
+        head.item = item;
+        size++;
+    }
+
+    @Override
+    public Item pop() {
+        /* TODO: implement it */
+        if (size>0) {
+            head = head.next;
+            size--;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new LinkedStackIterator();
+    }
+
+    private class LinkedStackIterator implements Iterator<Item> {
+        Node<Item> current = head;
+
+
+        @Override
+        public boolean hasNext() {
+            /* TODO: implement it */
+           if (current!=null)  return true;
+               return false;
+        }
+
+        @Override
+        public Item next() {
+            /* TODO: implement it */
+          Item item = current.item;
+            current = current.next;
+            return item;
+        }
+
+    }
+
+    private static class Node<Item> {
+        Item item;
+        Node<Item> next;
+
+        public Node(Item item, Node<Item> next) {
+            this.item = item;
+            this.next = next;
+        }
+    }
+}
